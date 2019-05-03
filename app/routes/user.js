2f2e6c85685e024aadebf5929969cd3459322d53
sdSalaat <asamadjhangir@gmail.com>
2019-05-03 13:39:17 +0500

    Test Api Added

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,6 +1,10 @@
 module.exports = (app, router) => {
     router.route('/user/getUsers')
         .get(app.controllers.getAllUsers);
+    router.route('/user/getUsers/once')
+        .get(app.controllers.getAllUsers);
+    router.route('/user/getUsers/twice')
+        .get(app.controllers.getAllUsers);
     router.route('/user/login')
         .post(app.controllers.loginUser);
     router.route('/user/register')
